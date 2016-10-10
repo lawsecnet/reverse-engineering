f83b22149c62045619e57cdac50eef14e1863fb8
lawsecnet <kamilbojarski7@gmail.com>
2016-10-10 19:20:59 +0200

    debugger initial files

---
+++ debugger/debugger_main.py
@@ -0,0 +1,11 @@
+from ctypes import *
+from debugger_define import *
+
+kernel32 = windll.kernel32
+
+class debugger():
+    def __init__(self):
+        pass
+
+    def load(self, path_to_file):
+        
