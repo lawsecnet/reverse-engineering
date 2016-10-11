7ff8c922bf80d314d47aa9d7a57f9c48bc196ef7
lawsecnet <kamilbojarski7@gmail.com>
2016-10-11 22:14:50 +0200

    debugger skeleton added

--- debugger/debugger_main.py
+++ debugger/debugger_main.py
@@ -8,4 +8,25 @@ class debugger():
         pass
 
     def load(self, path_to_file):
-        
+        # dwCreation flag determines how to create process
+        creation_flag = DEBUG_PROCESS
+
+        # initationg the structs
+        startupinfo = STARTUPINFO()
+        process_information = PROCESS_INFORMATION
+
+        # starting process in separate window
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
+
+        # initiazlizing cb variable in the STARTUPINFO struct
+        startupinfo.cb = sizeof(startupinfo)
+
+        if kernel32.CreateProcessA(path_to_exe, None, None, None, None,
+        creation_flags, None, None, byref(startupinfo), byref(process_information)):
+
+            print "Process launch successfull !!!"
+            print "> PID: %d" % process_information.dwProcessId
+
+        else:
+            print "[-] Error: 0x%08x." % kernel32.GetLastError()
